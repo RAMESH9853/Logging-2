3a0b0cae5b47115bd10c48da7bb7f8006c40b199
RAMESH9853 <123171941+RAMESH9853@users.noreply.github.com>
2023-03-29 11:23:23 +0530

    Add files via upload

---
+++ login_signup_project/website/signup/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+import mysql.connector as mysql
+fn=''
+ln=''
+s=''
+em=''
+pwd=''
+
+# Create your views here.
+def signaction(request):
+    global fn,ln,s,em,pwd
+    if request.method=="POST":
+        m=mysql.connect(host="localhost",user="root",password="root",database='website')
+        cursor=m.cursor()
+        d=request.POST
+        for key,value in d.items():
+            if key=="first_name":
+                fn=value
+            if key=="last_name":
+                ln=value
+            if key=="sex":
+                s=value
+            if key=="email":
+                em=value
+            if key=="password":
+                pwd=value
+
+        c="insert into users values('{}','{}','{}','{}','{}')".format(fn,ln,s,em,pwd)
+        cursor.execute(c)
+        m.commit()
+    
+    return render(request,'signup_page.html')
+
+    
